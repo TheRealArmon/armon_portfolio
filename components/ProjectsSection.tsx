af56b80712b8ed5b40694aa7db26e21c1539afc7
armon <armonah2@gmail.com>
2025-01-21 22:11:12 +0100

    refactor

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -1,28 +1,46 @@
+import { ExperiencesSection } from "./ExperiencesSection";
 import { ProjectCard } from "./ProjectCard";
 import { Icons } from "@/components/icons";
 
 export function ProjectsSection() {
+  const projects = [
+    {
+      title: "This website!",
+      role: "Developer",
+      description:
+        "Made this website in Next.js 15, mainly as a way of practicing front-end.\
+      Meant to act as a portfolio page, but I might also add blog posts or other features down the line.",
+      tags: ["next.js 15", "tailwind", "git"],
+      icon: <Icons.GlobalOutlined className="w-6 h-6 text-blue-400" />,
+    },
+    {
+      title: "Youtube videos",
+      role: "Filming, editing",
+      description:
+        "I create videos in my free-time! Not really technically relevant but fun nonetheless:)",
+      tags: ["hobby", "davinci resolve"],
+      icon: <Icons.SmileOutlined className="w-6 h-6 text-blue-400" />,
+    },
+    {
+      title: "Music",
+      role: "Amateur creator",
+      description:
+        "Calling myself a \"music producer\" would be wildly overselling, that's why I've written amateur creator instead!\
+        Spurred by sporadic bursts of inspiration, I keep coming back to this hobby.",
+      tags: ["hobby", "ableton"],
+      icon: <Icons.SmileOutlined className="w-6 h-6 text-blue-400" />,
+    },
+  ];
+
   return (
     <section id="projects" className="py-20 px-4 bg-[#232936]">
       <div className="max-w-6xl mx-auto">
         <h1 className="text-5xl font-bold text-center mb-16">Projects</h1>
 
         <div className="grid md:grid-cols-2 gap-6">
-          <ProjectCard
-            title="This website!"
-            role="Developer"
-            description="Made this website in Next.js 15, mainly as a way of practicing front-end.
-            Mainly meant to act as a portfolio page, but I might also add blog posts or other features down the line."
-            tags={["next.js 15", "tailwind", "git"]}
-            icon={<Icons.GlobalOutlined className="w-6 h-6 text-blue-400" />}
-          />
-          <ProjectCard
-            title="Youtube videos"
-            role="Filming, editing"
-            description="I create videos in my free-time! Not really technically relevant but fun nonetheless:)"
-            tags={["hobby", "davinci resolve"]}
-            icon={<Icons.SmileOutlined className="w-6 h-6 text-blue-400" />}
-          />
+          {projects.map((project, index) => (
+            <ProjectCard key={index} {...project} />
+          ))}
         </div>
       </div>
     </section>
--- components/icons.tsx
+++ components/icons.tsx
@@ -1,24 +1,30 @@
-import { SmileOutlined, GlobalOutlined } from '@ant-design/icons'
-import { DockerOutlined, KubernetesOutlined, LinuxOutlined, PythonOutlined } from '@ant-design/icons'
-import Image, { ImageProps } from 'next/image'
+import {
+  SmileOutlined,
+  GlobalOutlined,
+  DockerOutlined,
+  KubernetesOutlined,
+  LinuxOutlined,
+  PythonOutlined,
+} from "@ant-design/icons";
+import Image, { ImageProps } from "next/image";
 
 export const Icons = {
-    SmileOutlined: SmileOutlined,
-    GlobalOutlined: GlobalOutlined,
-    DockerOutlined: DockerOutlined,
-    KubernetesOutlined: KubernetesOutlined,
-    LinuxOutlined: LinuxOutlined,
-    PythonOutlined: PythonOutlined,
-}
+  SmileOutlined: SmileOutlined,
+  GlobalOutlined: GlobalOutlined,
+  DockerOutlined: DockerOutlined,
+  KubernetesOutlined: KubernetesOutlined,
+  LinuxOutlined: LinuxOutlined,
+  PythonOutlined: PythonOutlined,
+};
 
 export const Logos = {
-    Go: (props: Omit<ImageProps, 'src' | 'alt'>) => (
-        <Image
-            src="/img/go-logo.png"
-            alt="go-logo"
-            width={32} // This will be applied if not overridden
-            height={32}
-            {...props}
-        />
-    ),
-}
+  Go: (props: Omit<ImageProps, "src" | "alt">) => (
+    <Image
+      src="/img/go-logo.png"
+      alt="go-logo"
+      width={32} // This will be applied if not overridden
+      height={32}
+      {...props}
+    />
+  ),
+};
